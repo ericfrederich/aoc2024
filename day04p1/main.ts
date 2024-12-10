67d09cfc6e6de0970a9a0f4b4ffb0b8191cf2951
Eric L. Frederich <eric.frederich@gmail.com>
2024-12-09 20:04:35 -0500

    Told copilot it was wrong again.
    Exact prompt:
    Now it's finding 26 instead of 8. I give up. I am not in fear of my day job if you can't complete this simple task.

--- day04p1/main.ts
+++ day04p1/main.ts
@@ -10,48 +10,49 @@ export function xmasSearch(data: string[]): number {
 
   const searchPatterns = [xmas, xmas.split("").reverse().join("")];
 
-  const searchInDirection = (getChars: (i: number, j: number) => string, limit: number, innerLimit: number) => {
-    for (const searchWord of searchPatterns) {
-      for (let i = 0; i < limit; i++) {
-        let chars = "";
-        for (let j = 0; j < innerLimit; j++) {
-          chars += getChars(i, j);
-        }
-        ret += occurrences(chars, searchWord);
-      }
-    }
-  };
-
   // Horizontal and vertical searches
-  searchInDirection((i, j) => data[i][j], height, width); // Horizontal
-  searchInDirection((i, j) => data[j][i], width, height); // Vertical
+  for (const searchWord of searchPatterns) {
+    for (let i = 0; i < height; i++) {
+      ret += occurrences(data[i], searchWord); // Horizontal
+    }
+    for (let j = 0; j < width; j++) {
+      let vertical = "";
+      for (let i = 0; i < height; i++) {
+        vertical += data[i][j];
+      }
+      ret += occurrences(vertical, searchWord); // Vertical
+    }
+  }
 
   // Diagonal searches
   const searchDiagonals = (startRow: number, startCol: number, rowInc: number, colInc: number) => {
-    for (let i = 0; i < height; i++) {
-      let chars = "";
-      for (let j = 0; j < width; j++) {
-        const row = startRow + i * rowInc;
-        const col = startCol + j * colInc;
-        if (row >= 0 && row < height && col >= 0 && col < width) {
-          chars += data[row][col];
-        } else {
-          break;
-        }
-      }
-      ret += occurrences(chars, xmas);
-      ret += occurrences(chars, xmas.split("").reverse().join(""));
+    let chars = "";
+    let row = startRow;
+    let col = startCol;
+    while (row >= 0 && row < height && col >= 0 && col < width) {
+      chars += data[row][col];
+      row += rowInc;
+      col += colInc;
     }
+    return chars;
   };
 
-  // Diagonal (\) and (/)
-  for (let i = 0; i <= height - xmas.length; i++) {
-    searchDiagonals(i, 0, 1, 1); // Diagonal (\) from left side
-    searchDiagonals(i, 0, -1, 1); // Diagonal (/) from left side
-  }
-  for (let j = 1; j <= width - xmas.length; j++) {
-    searchDiagonals(0, j, 1, 1); // Diagonal (\) from top side
-    searchDiagonals(height - 1, j, -1, 1); // Diagonal (/) from bottom side
+  for (const searchWord of searchPatterns) {
+    // Diagonal (\)
+    for (let i = 0; i <= height - xmas.length; i++) {
+      ret += occurrences(searchDiagonals(i, 0, 1, 1), searchWord); // From left side
+    }
+    for (let j = 1; j <= width - xmas.length; j++) {
+      ret += occurrences(searchDiagonals(0, j, 1, 1), searchWord); // From top side
+    }
+
+    // Diagonal (/)
+    for (let i = 0; i <= height - xmas.length; i++) {
+      ret += occurrences(searchDiagonals(i, 0, -1, 1), searchWord); // From left side
+    }
+    for (let j = 1; j <= width - xmas.length; j++) {
+      ret += occurrences(searchDiagonals(height - 1, j, -1, 1), searchWord); // From bottom side
+    }
   }
 
   return ret;
