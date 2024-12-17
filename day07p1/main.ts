d16782a834f864a03497425deed4177783a87383
Eric L. Frederich <eric.frederich@gmail.com>
2024-12-17 11:53:52 -0500

    Day 7 part 1

--- day07p1/main.ts
+++ day07p1/main.ts
@@ -1,8 +1,49 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { set, single } from "npm:itertools-ts";
+type Equation = { result: number; operands: readonly number[] };
+
+export function parseEquations(input: string): Equation[] {
+  const lines = input.split("\n").filter((line) => line.trim());
+  return lines.map((line) => line.split(": ")).map((
+    [resultStr, operandsStr],
+  ) => ({
+    result: Number(resultStr),
+    operands: operandsStr.split(" ").map(Number),
+  }));
+}
+
+export function bridgeRepair(equations: Equation[]): number {
+  let ret = 0;
+  for (const { result, operands } of equations) {
+    const operatorOptions = Array.of(
+      ...set.cartesianProduct(...single.repeat("+*", operands.length - 1)),
+    );
+    for (const operators of operatorOptions) {
+      const operandStack = operands.toReversed();
+      const operatorStack = Array.of(...operators);
+      while (operatorStack.length > 0) {
+        const [left, right] = [operandStack.pop()!, operandStack.pop()!];
+        const operator = operatorStack.pop();
+        let computed: number = 0;
+        if (operator === "*") {
+          computed = left * right;
+        } else if (operator === "+") {
+          computed = left + right;
+        }
+        operandStack.push(computed);
+      }
+      const final = operandStack.pop();
+      if (final === result) {
+        ret += final;
+        break;
+      }
+    }
+  }
+  return ret;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const bytes = await Deno.readFile("input.txt");
+  const fileStr = new TextDecoder().decode(bytes).trimEnd();
+  console.log(bridgeRepair(parseEquations(fileStr)));
 }
