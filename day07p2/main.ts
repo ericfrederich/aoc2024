7749671a712f60566fc5aa389d0456c2429eac9f
Eric L. Frederich <eric.frederich@gmail.com>
2024-12-17 14:28:18 -0500

    refactor

--- day07p2/main.ts
+++ day07p2/main.ts
@@ -11,25 +11,26 @@ export function parseEquations(input: string): Equation[] {
   }));
 }
 
+const opFuncs: { [key: string]: (l: number, r: number) => number } = {
+  "*": (left: number, right: number) => left * right,
+  "+": (left: number, right: number) => left + right,
+  "|": (left: number, right: number) => Number(String(left) + String(right)),
+};
+
 export function bridgeRepair(equations: Equation[]): number {
   let ret = 0;
   for (const { result, operands } of equations) {
-    const operatorOptions = set.cartesianProduct(...single.repeat("+*|", operands.length - 1));
+    const operatorOptions = set.cartesianProduct(
+      ...single.repeat("+*|", operands.length - 1),
+    );
     const operandsReversed = operands.toReversed();
     for (const operators of operatorOptions) {
       const operandStack = Array.from(operandsReversed);
       const operatorStack = Array.from(operators);
       while (operatorStack.length > 0) {
         const [left, right] = [operandStack.pop()!, operandStack.pop()!];
-        const operator = operatorStack.pop();
-        let computed: number = 0;
-        if (operator === "*") {
-          computed = left * right;
-        } else if (operator === "+") {
-          computed = left + right;
-        } else if (operator === "|") {
-          computed = Number(String(left) + String(right));
-        }
+        const operator = operatorStack.pop()!;
+        const computed = opFuncs[operator](left, right);
         operandStack.push(computed);
       }
       const final = operandStack.pop();
